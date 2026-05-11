16e6a004aa42e979607dec4b8554053d2572bb2d
dev_leo <146463507+lxzzt@users.noreply.github.com>
2026-05-11 20:13:00 -0300

    V 01
    Apenas a classe extrator pronta,classe que apenas extrai os dados brutos da api para serem tratados.arrumei para a convencção python,antes estava na conveção java.

--- src/extrator.py
+++ src/extrator.py
@@ -1,19 +1,19 @@
 import requests
 import time
-
-
 class Extrator:
     def __init__(self):
         self.baseUrl = "https://fakestorageapi.com"
     def fetch_endpoint(self,endpoint,max_retrives=3):
-        for i in (max_retrives):
+        for i in range(max_retrives):
             try:
-                response = requests.get(f"{self.baseUrl}/{endpoint}")
-                if response.statusCode == 200:
+                response = requests.get(f"{self.base_url}/{endpoint}")
+                if response.status_code == 200:
                     return response.json()
                 else:
-                    print(f"Erro{response.statusCode},ao buscar{endpoint},foram {i+1} tentativas!")
+                    print(f"Erro{response.status_code},ao buscar{endpoint},foram {i+1} tentativas!")
+
             except Exception as e:
-                print("Falha De Conexão {e}")
+                print(f"Falha De Conexão {e}")
+
             time.sleep(1)
-            return None
+        return None
